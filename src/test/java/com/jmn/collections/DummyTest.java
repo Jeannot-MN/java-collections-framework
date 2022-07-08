fca031fa7130ecf530a9833877c596c57b7a42d5
JeannotMN <jeannot@ssquared.dev>
2022-07-08 08:23:55 +0200

    chore: Set up (junit, assertj).

--- src/test/java/com/jmn/collections/DummyTest.java
+++ src/test/java/com/jmn/collections/DummyTest.java
@@ -3,7 +3,6 @@ package com.jmn.collections;
 import com.sun.org.slf4j.internal.Logger;
 import com.sun.org.slf4j.internal.LoggerFactory;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,14 +10,9 @@ public class DummyTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(DummyTest.class);
 
-    @BeforeAll
-    static void setup() {
-        LOG.debug(String.format("Executing %s class tests.", DummyTest.class));
-    }
-
     @Test
     @DisplayName("Dummy Junit5 test")
-    void shouldTest(){
+    void shouldTest() {
         Assertions.assertThat(2).isEqualTo(2);
     }
 }
