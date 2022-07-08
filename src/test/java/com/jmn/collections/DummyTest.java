b69e192072c2dae8f5e50ccd752baa774eda504a
JeannotMN <jeannot@ssquared.dev>
2022-07-08 08:24:12 +0200

    chore: Set up (junit, assertj).

--- src/test/java/com/jmn/collections/DummyTest.java
+++ src/test/java/com/jmn/collections/DummyTest.java
@@ -1,14 +1,10 @@
 package com.jmn.collections;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DummyTest {
-
-    private static final Logger LOG = LoggerFactory.getLogger(DummyTest.class);
 
     @Test
     @DisplayName("Dummy Junit5 test")
